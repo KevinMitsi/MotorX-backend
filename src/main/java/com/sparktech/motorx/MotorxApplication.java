89b675252766e83ab67711a075e8867f370191a0
Kevinmitsi <kevinandresgarciaaguirre@gmail.com>
2026-02-16 14:44:05 -0500

    first commit

---
+++ src/main/java/com/sparktech/motorx/MotorxApplication.java
@@ -0,0 +1,13 @@
+package com.sparktech.motorx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotorxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparktech/motorx/MotorxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparktech.motorx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
