e27742925fba784a5f34e5cd9ddd145e75204620
Kevinmitsi <kevinandresgarciaaguirre@gmail.com>
2026-03-01 13:23:24 -0500

    Changing properties

--- src/test/java/com/sparktech/motorx/MotorxApplicationTests.java
+++ src/test/java/com/sparktech/motorx/MotorxApplicationTests.java
@@ -1,7 +1,6 @@
 package com.sparktech.motorx;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -9,9 +8,8 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("test")
 class MotorxApplicationTests {
 
-    @Test
-    @DisabledIfEnvironmentVariable(named = "CI", matches = "true")
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
